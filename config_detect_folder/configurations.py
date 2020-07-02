eee9ffa2c9e3ed2b89a72b566c8ae45d1bbcc0c9
YashasPSwamy <47856570+yashasps@users.noreply.github.com>
2020-07-02 15:57:03 +0530

    uploading all

---
+++ config_detect_folder/configurations.py
@@ -0,0 +1,8 @@
+MODEL_PATH = "yolo-coco" #path to model
+
+# initializes minimum probabilities to filter weak detections along with the threshold when applying non-maxima suppression
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
+
+# define the minimum safe distance (in pixels) that two people can befrom each other
+MIN_DISTANCE = 120
